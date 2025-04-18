7c89fe39b6923a8ead028644ab82bd092caca7f5
KAMIKADZ4 <kamikadz4@yandex.ru>
2025-04-18 16:25:24 +0300

    lint

--- cmd/auth/auth_service.go
+++ cmd/auth/auth_service.go
@@ -1,35 +1,34 @@
-package main 
+package main
 
 import (
-  "os"
-  "log"
-  "github.com/gofiber/fiber/v3"
-  "github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"log"
+	"os"
 )
 
 func okHandler(context fiber.Ctx) error {
-  return context.JSON(fiber.Map{
-    "status": "OK",
-    "service": "auth",
-  })
+	return context.JSON(fiber.Map{
+		"status":  "OK",
+		"service": "auth",
+	})
 }
 
-
 func main() {
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "5000"
-  }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
 
-  app := fiber.New()
+	app := fiber.New()
 
-  app.Use(logger.New(logger.Config{
-    Format: "${time} - [${method} ${status}] ${latency} ${url}\n",
-  }))
+	app.Use(logger.New(logger.Config{
+		Format: "${time} - [${method} ${status}] ${latency} ${url}\n",
+	}))
 
-  app.Get("/health-check", okHandler )
-  app.Get("/ready-check", okHandler)
+	app.Get("/health-check", okHandler)
+	app.Get("/ready-check", okHandler)
 
-  log.Println("Lets go!")
-  log.Fatal(app.Listen(":" + port))
+	log.Println("Lets go!")
+	log.Fatal(app.Listen(":" + port))
 }
--- cmd/users/users_service.go
+++ cmd/users/users_service.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "net/http"
-  "os"
-  "log"
+	"log"
+	"net/http"
+	"os"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func okHandler(c *gin.Context) {
-  c.JSON(http.StatusOK, gin.H{
-    "status": "OK",
-    "service": "users",
-  })
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "OK",
+		"service": "users",
+	})
 }
 
 func main() {
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "5003"
-  }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5003"
+	}
 
-  r := gin.Default()
-  r.GET("/health-check", okHandler)
-  r.GET("/ready-check", okHandler)
+	r := gin.Default()
+	r.GET("/health-check", okHandler)
+	r.GET("/ready-check", okHandler)
 
-  log.Println("Lets go!")
-  r.Run("localhost:" + port)
+	log.Println("Lets go!")
+	r.Run("localhost:" + port)
 }
